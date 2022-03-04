ae4b6643e759af2ca5d5bf013e5b62f45fc7554e
arxxv <arnav.shindolkar0505@gmail.com>
2022-03-04 11:45:10 +0530

    cache fix

--- utils/redisHelper.js
+++ utils/redisHelper.js
@@ -1,36 +1,40 @@
 const Redis = require("redis");
 require("dotenv").config();
 const redisClient = Redis.createClient({
-  host: process.env.REDISHOST,
-  port: process.env.PORT,
+  url: "redis://default:dES3m5mD2Y0OtLx6tdavzipFNzzl5JF1@redis-11538.c8.us-east-1-3.ec2.cloud.redislabs.com:11538",
 });
 const DEF_EXP_TIME = 3600;
+console.log(typeof process.env.REDISPORT);
 
 module.exports.checkCache = async (key, cb) => {
   try {
+    console.log("######");
     await redisClient.connect();
-  } catch (error) {}
-  return new Promise(async (resolve, reject) => {
-    const data = await redisClient.get(key);
-    if (data) return resolve(JSON.parse(data));
-    const newData = await cb();
-    try {
-      await redisClient.setEx(key, DEF_EXP_TIME, JSON.stringify(newData));
-    } catch (error) {}
-    resolve(newData);
-  });
+    console.log("######");
+    return new Promise(async (resolve, reject) => {
+      const data = await redisClient.get(key);
+      if (data) return resolve(JSON.parse(data));
+      const newData = await cb();
+      try {
+        await redisClient.setEx(key, DEF_EXP_TIME, JSON.stringify(newData));
+      } catch (error) {}
+      resolve(newData);
+    });
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 module.exports.setCache = async (key, data) => {
   try {
     await redisClient.connect();
+    await redisClient.set(key, JSON.stringify(data));
   } catch (error) {}
-  await redisClient.set(key, JSON.stringify(data));
 };
 
 module.exports.deleteCache = async (key) => {
   try {
     await redisClient.connect();
+    await redisClient.del(key);
   } catch (error) {}
-  await redisClient.del(key);
 };
